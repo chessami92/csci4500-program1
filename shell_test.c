9ca056bde9eaee0cea7d52036d04a5c005cf1f24
Josh DeWitt <jdewitt@unomaha.edu>
2013-05-16 23:45:44 -0500

    Condensed all parsing tests into one.

--- shell_test.c
+++ shell_test.c
@@ -12,28 +12,18 @@ static void test_input() {
 static void test_parse() {
     char line[30];
     char *words[10];
-    strcpy( line, "testing\t\t parse |\tfunction\t   \t" );
 
+    strcpy( line, "testing\t\t parse |\tfunction\t   \t" );
     assert( parse( 10, 10, line, words ) == 1 && "Parse seems to have failed" );
     assert( strcmp( words[0], "testing" ) == 0 );
     assert( strcmp( words[1], "parse" ) == 0 );
     assert( strcmp( words[2], "|" ) == 0 );
     assert( strcmp( words[3], "function" ) == 0 );
-}
 
-static void test_parse_long_word() {
-    char line[30];
-    char *words[10];
     strcpy( line, "testingLongWord" );
-
     assert( parse( 10, 10, line, words ) == 0 && "Should have failed because of long word." );
-}
 
-static void test_parse_too_many_words() {
-    char line[30];
-    char *words[10];
     strcpy( line, "0 1 2 3 4 5 6 7 8 9 10" );
-
     assert( parse( 10, 10, line, words ) == 0 && "Should have failed because of long word." );
 }
 
@@ -65,8 +55,6 @@ static void test_getPath() {
 int main( void ) {
     test_input();
     test_parse();
-    test_parse_long_word();
-    test_parse_too_many_words();
     test_getPath();
     return 0;
 }   
