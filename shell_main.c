075354b82ff01b1ae1dddb37375cd7c9bb7fe5c7
Josh DeWitt <jdewitt@unomaha.edu>
2013-05-20 21:18:40 -0500

    Added in checking for EOF, instead of exit.

--- shell_main.c
+++ shell_main.c
@@ -3,6 +3,7 @@
 #define MAX_LINE_LENGTH 100
 #define MAX_WORDS 16
 #define MAX_WORD_LENGTH 64
+#define EOF 4
 
 //Main function for the shell. Gets input, parses it, then executes it.
 int main(int argc, char *argv[]) {
@@ -17,14 +18,13 @@ int main(int argc, char *argv[]) {
             continue;
         }
  
+        if( line[0] == EOF ) {  /* Check for the EOF character. */
+            return( 0 );
+        }
+
         /* Parse into individual words. */
         if( !parse( MAX_WORDS, MAX_WORD_LENGTH, line, words ) ) {
             continue;           /* Do not notify user - they were given the issue already. */
-        }
-
-        /* Check if use is exiting the shell. */
-        if( strcmp( words[0], "exit" ) == 0 ) {
-            return( 0 );
         }
         
         execute( words );       /* Execute the parsed commands */
